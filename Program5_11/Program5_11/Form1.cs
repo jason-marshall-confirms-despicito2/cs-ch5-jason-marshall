ee67e7894ba0af003a76af92fa8f30764cd5702c
jason-marshall-confirms-despicito2 <43045279+jason-marshall-confirms-despicito2@users.noreply.github.com>
2018-11-05 14:34:26 -0500

    Completed 5-3 & 5-5

---
+++ Program5_11/Program5_11/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Program5_11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Create a random object named rand and assign it a random integer
+        Random rand = new Random();
+
+        // Declare three integers: n, guess, and attempt
+        int n, guess, attempt;
+
+        // Before the form loads
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Call the getNumber method
+            getNumber();
+        }
+
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            // If txtNumber.Text can be parsed as an integer, assign that value to guess
+                // Increment attempt
+                // If guess = n
+
+                    /* Output to lblOutput the following:
+                     * 
+                     * "You tried " + attempt + " time(s). \n" +
+                     * "Your last guess is " + guess + "\n" +
+                     * "Congratulations, you won! Enter a number to start again."
+                     * 
+                     */
+                     // Clear txtNumber
+                     // Call the getNumber method
+                 
+                // else if guess > n
+                    
+                    /* Output to lblOutput the following:
+                     * 
+                     * You tried " + attempt + " time(s).\n" + guess + " is too high. Try again."
+                     * 
+                     */
+                     // Clear txtNumber
+
+                // else
+
+                    /* Output to lblOutput the following:
+                     * 
+                     * "You tried " + attempt + " time(s).\n" + guess + is too low, try again."
+                     * 
+                     */
+                     // clear txtNumber
+            }
+        }
+
+        // This is the getNumber method
+        public void getNumber()
+        {
+            // Assign a random integer between 1 and 100 to integer n
+            // Set attempt to 0
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,29 @@
+# Read all of the instructions in this file before doing Anything.
+
+## C# - End of Chapter Programming Problems for Chapter 5
+
+#### Welcome to the programming assignment for the end of Chapter 5
+
+In this repo, you will find three folders. Each of them is a partially built program.
+
+Your job is to complete the partially built program following the instructions inside of the form1.cs file.
+
+### Instructions to get to the instructions
+
+1. ***Clone*** this repo to your **S:\ Drive** using **Github Desktop**
+2. ***Open*** the **.sln** File for the problem that you want to work on
+3. In the ***Solution Explorer*** right click on **Form1.cs** and select **View Code**
+4. Repeat step 3 and select **View Designer**
+5. Follow the instructions notated by the **"//" and "/*"** Marks
+
+### Instructions to turn in your assignment
+
+1. In each of your open files press *ctrl + S* to **save** your files
+2. **Open Github Desktop** and make sure that it is pointed at the **correct repo**
+3. Input your **description** (think title) and **summary** (think notes of what you did)
+4. Click the blue ***Commit*** button and **wait**
+5. Click the dark grey ***Push*** in the top bar and **wait**
+6. Go to *your own repo* for the assignment on ***Github.com*** and make sure your files are there
+7. **Copy** your repo link and attach it to your assignment in **Schoology**
+8. Click the **Submit Button** in **Schoology**
+9. Wait for your grade to show up in **Schoology** and **Skyward**
